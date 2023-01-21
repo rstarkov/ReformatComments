208fb767cd6aecd57571ef65fffcebaebcbeae4b
Roman Starkov <sourcecode@roman.st>
2023-01-21 01:00:04 +0000

    Split from RT.VisualStudio as-is

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using RT.VisualStudio;
+
+namespace ReformatComments;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var code = File.ReadAllText(args[0]);
+        code = CommentFormatter.ReformatComments(code);
+        File.Copy(args[0], args[0] + ".bak", overwrite: true);
+        File.WriteAllText(args[0], code);
+    }
+}
