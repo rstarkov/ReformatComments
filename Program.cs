1c5fd39bb74d75414cf18abde99bb8f7f083bbd3
Roman Starkov <sourcecode@roman.st>
2023-08-22 11:36:39 +0100

    Support a backup path instead of a local backup

--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
-﻿using RT.VisualStudio;
+﻿using RT.Util;
+using RT.VisualStudio;
 
 namespace ReformatComments;
 
 internal class Program
 {
-    static void Main(string[] args)
+    static int Main(string[] args)
     {
+        if (args.Length == 0)
+        {
+            Console.WriteLine($"Usage: ReformatComments.exe <file.cs> [<backup-path>]");
+            return -1;
+        }
         var code = File.ReadAllText(args[0]);
         code = CommentFormatter.ReformatComments(code);
-        File.Copy(args[0], args[0] + ".bak", overwrite: true);
+        var backupPath = args[0] + ".bak";
+        if (args.Length >= 2)
+        {
+            Directory.CreateDirectory(args[1]);
+            backupPath = Path.Combine(args[1], PathUtil.AppendBeforeExtension(Path.GetFileName(args[0]), $".{DateTime.Now:yyyy-MM-dd--HH.mm.ss.fff}"));
+        }
+        File.Copy(args[0], backupPath, overwrite: true);
         File.WriteAllText(args[0], code);
+        return 0;
     }
 }
